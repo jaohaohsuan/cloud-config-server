739fb9da429b4808345294dc890fafe87ce09d8f
henry <jaohaohsuan@gmail.com>
2016-01-29 10:40:47 +0800

    use sed subst var

--- root/master.sh
+++ root/master.sh
@@ -4,8 +4,8 @@
 myip="192.168.31.50"
 dns="192.168.31.1"
 gateway="192.168.31.1"
-etcd1="192.168.31.30"
-etcd2="192.168.31.31"
-etcd3="192.168.31.32"
+etcdip1="192.168.31.30"
+etcdip2="192.168.31.31"
+etcdip3="192.168.31.32"
 
-sed -e "s/\${myip}/192.168.31.50/" -e "s/\${dns}/192.168.31.1/" -e "s/\${etcdip1}/192.168.31.30/" -e "s/\${etcdip2}/192.168.31.31/" -e "s/\${etcdip3}/192.168.31.32/" -e "s/\${gateway}/192.168.31.1/"  $1
+sed -e "s/\${myip}/$myip/" -e "s/\${dns}/$dns/" -e "s/\${etcdip1}/$etcdip1/" -e "s/\${etcdip2}/$etcdip2/" -e "s/\${etcdip3}/$etcdip3/" -e "s/\${gateway}/$gateway/" $1
